2ba2725045bfe66f24c5be2a9dbefc36522bb2ed
Raro Macias <ggmacias@gmail.com>
2022-10-03 15:20:20 -0400

    fixed hex links

--- client/hex.js
+++ client/hex.js
@@ -1,11 +1,11 @@
 const hexBtn = document.getElementById('hexBtn');
 const hex = [0,1,2,3,4,5,6,7,8,9, 'A','B','C','D','E','F']
 const newColor = document.querySelector('.newColor')
-const navToggle = document.querySelector('.nav-toggle')
-const links = document.querySelector('.links')
+const hexNavToggle = document.querySelector('.hex-nav-toggle')
+const hexLinks = document.querySelector('.hex-links')
 
-function playLinks(){
-   links.classList.toggle('show-links')
+function pressLinks(){
+   hexLinks.classList.toggle('show-hexLinks')
 }
 
 function hexChange (){
@@ -22,4 +22,4 @@ function getRandomNumberHex(){
 }
 
 hexBtn.addEventListener('click', hexChange)
-navToggle.addEventListener("click", playLinks)
+hexNavToggle.addEventListener("click", pressLinks)
